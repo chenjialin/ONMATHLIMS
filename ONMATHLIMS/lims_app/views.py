c354d14a693009faedf2738c50d2ed684acaf081
chencheng <291552579@qq.com>
2017-07-15 10:32:40 +0800

    add main view render all projects

--- ONMATHLIMS/lims_app/views.py
+++ ONMATHLIMS/lims_app/views.py
@@ -5,8 +5,10 @@ from django.http import HttpResponse, HttpResponseRedirect
 from forms import SampleProjectMasterForm, UserForm
 from django.core.urlresolvers import reverse
 from django.shortcuts import render
-from models import UserInfo
+from models import UserInfo, SampleProjectMaster
+
 import os
+import json
 # Create your views here.
 CODE_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -45,11 +47,32 @@ def project_input(request):
 
 def main(request):
     username = request.COOKIES.get('username', '')
+    all_projects = SampleProjectMaster.objects.all()
+    all_projects_list = []
+    for each_project in all_projects:
+        project_dict = {}
+        project_dict['id'] = each_project.id
+        project_dict['project_number'] = each_project.project_number
+        project_dict['create_time'] = each_project.create_time.split(" ")[0]
+        project_dict['status'] = each_project.status
+        project_dict['cust_user'] = each_project.cust_user
+        all_projects_list.append(project_dict)
     return render(request, os.path.join(CODE_ROOT, 'lims_app/templates', 'index.html'),
-                  {'username': username})
+                  {'username': username, 'all_projects': all_projects_list})
 
 
 def receive_sample(request):
     username = request.COOKIES.get('username', '')
     return render(request, os.path.join(CODE_ROOT, 'lims_app/templates', 'receive_sample.html'),
                   {'username': username})
+
+
+def project_view(request):
+    if request.method == 'POST':
+        action = request.POST['action']
+        '''
+        if action == 'edit':
+            project_master_info =
+        '''
+        data = " ".join(['ajax', 'work!', 'action', 'is', action])
+    return HttpResponse({json.dumps(data)})
