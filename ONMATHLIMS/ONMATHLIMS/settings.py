32c065da7dae6f533d26d48abae48da685b11aad
chencheng <291552579@qq.com>
2017-07-24 17:20:33 +0800

    change mysql passwd and user

--- ONMATHLIMS/ONMATHLIMS/settings.py
+++ ONMATHLIMS/ONMATHLIMS/settings.py
@@ -84,8 +84,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'SEQ_SA_INFO',                      # Or path to database file if using sqlite3.
-        'USER': 'xxxxxxxxxxxxxxxxxxxxxxxxx',
-        'PASSWORD': 'xxxxxxxxxxxxxxxxxxxxxxx',
+        'USER': 'root',
+        'PASSWORD': '050400',
         'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306',                      # Set to empty string for default.
     }
