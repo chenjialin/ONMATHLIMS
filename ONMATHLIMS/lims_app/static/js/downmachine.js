21ba6f8fca6affc2767132360f563987443cf939
chencheng <291552579@qq.com>
2017-08-02 16:28:24 +0800

    add 5 tables jqgrid

---
+++ ONMATHLIMS/lims_app/static/js/downmachine.js
@@ -0,0 +1,25 @@
+$(function(){
+  pageInit();
+});
+function pageInit(){
+  $("#table").jqGrid(
+    {
+      url: 'save',
+      datatype : 'local',
+      height: 500,
+      // height: 'auto',
+      colNames: ['样品名称', '样品编号', '数据量', 'q20', 'q30', '时间', '备注'],
+      colModel: [
+            { name: 'sample_name', index: 'sample_name', editable: true, width: 60},
+            { name: 'sample_id', index: 'sample_id', editable: true, width: 60},
+            { name: 'data_count', index: 'data_count', editable: true, width: 60},
+            { name: 'q20', index: 'q20', editable: true, width: 60},
+            { name: 'q30', index: 'q30', editable: true, width: 60},
+            { name: 'time', index: 'time', editable: true, width: 60},
+            { name: 'comment', index: 'comment', editable: true, width: 60}
+          ],
+      gridComplete: function() {},
+      caption : "上机结果",//表格的标题名字
+      width: 1000
+    });
+}
---
+++ ONMATHLIMS/lims_app/static/js/quality_check.js
@@ -0,0 +1,26 @@
+$(function(){
+  pageInit();
+});
+function pageInit(){
+  $("#table").jqGrid(
+    {
+      url: 'save',
+      datatype : 'local',
+      height: 500,
+      // height: 'auto',
+      colNames: ['样品名称', '样品编号', '浓度', '体积', 'RIN值', '判定结果', '时间', '备注'],
+      colModel: [
+            { name: 'sample_name', index: 'sample_id', editable: true, width: 60},
+            { name: 'sample_id', index: 'sample_id', editable: true, width: 60},
+            { name: 'concentration', index: 'concentration', editable: true, width: 60},
+            { name: 'volume', index: 'volume', editable: true, width: 60},
+            { name: 'rin', index: 'rin', editable: true, width: 60},
+            { name: 'results', index: 'results', editable: true, width: 60},
+            { name: 'time', index: 'time', editable: true, width: 60},
+            { name: 'comment', index: 'comment', editable: true, width: 60}
+          ],
+      gridComplete: function() {},
+      caption : "质检结果",//表格的标题名字
+      width: 1000
+    });
+}
