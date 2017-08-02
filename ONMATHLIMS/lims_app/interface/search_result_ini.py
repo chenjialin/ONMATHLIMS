1e95ad16d35a95d2622dffa23ae858789092f458
jedrek <839588325@qq.com>
2017-08-02 23:38:53 +0800

    fix bugs.

--- ONMATHLIMS/lims_app/interface/search_result_ini.py
+++ ONMATHLIMS/lims_app/interface/search_result_ini.py
@@ -3,9 +3,6 @@ from django.db import connection
 
 
 def get_search_result(search_key):
-    search_type = 'sample'
-    if 'ONMATH' in search_key:
-        search_type = 'project'
     cursor = connection.cursor()
     sql = """SELECT
             m.project_name as "项目名称",
@@ -13,18 +10,22 @@ def get_search_result(search_key):
             m.status as "项目状态",
             m.create_time as "项目创建时间",
             m.cust_user as "客户",
+            s.species as "物种",
             d.sample_name as "样品名称",
+            d.sample_id as "样品ID",
             d.sendsample_time as "送样时间",
             d.downmachine_num as "下机时间"
              FROM sample_project_master m
              inner join sample_info_detail d on d.project_id=m.id
+             inner join send_sample s on s.project_id=m.id
     """
-    condition = " where m.project_number='%s'" if search_type == 'project' else " where d.sample_name='%s'"
-    sql += condition % search_key
+    condition = " where m.project_number='{key}' or d.sample_name='{key}' or d.sample_id='{key}' " \
+                "or s.species='{key}' limit 1000"
+    sql += condition.format(key=search_key)
     cursor.execute(sql)
     results = cursor.fetchall()
 
-    table_header = ['项目名称', '项目编码', '项目状态', '项目创建时间', '客户', '样品名称', '送样时间', '下机时间']
+    table_header = ['项目名称', '项目编码', '项目状态', '项目创建时间', '客户', '物种', '样品名称', '样品ID', '送样时间', '下机时间']
     table_values = results
 
     return table_header, table_values
