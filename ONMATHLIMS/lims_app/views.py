7abba0483d6808e4fabc5fcfb61d7856569d8c28
chencheng <291552579@qq.com>
2017-08-02 16:40:46 +0800

    add upload data table and apply split database to each view

--- ONMATHLIMS/lims_app/views.py
+++ ONMATHLIMS/lims_app/views.py
@@ -5,14 +5,14 @@ import sys
 import json
 import datetime
 
-from . import DbObjectDoesNotExist, select_colums_dict
+from . import DbObjectDoesNotExist
 sys.path.append('/usr/local/lib/python2.7/dist-packages')
 import django_excel as excel
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from forms import SampleProjectMasterForm, UserForm
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, redirect
-from models import SampleProjectMaster, SampleInfoDetail, Attachment
+from models import SampleProjectMaster, SampleInfoDetail, Attachment, SendSample, QualityCheck, BuildLib, UpMachine, DownMachine
 from interface import search_result_ini, get_sample_info, common
 
 
@@ -92,7 +92,7 @@ def operation_log(request):
 @login_required
 def main(request):
     username = request.COOKIES.get('username', '')
-    #login_serach(request, username)
+
     if 'search' in request.GET.keys() or 'q' in request.GET.keys():
         key_word = request.GET.get('q')
         return redirect('/lims_app/search?q=%s' % key_word)
@@ -130,7 +130,7 @@ view function for onmathlims modules
 
 
 @login_required
-def receive_sample(request):
+def send_sample(request):
     username = request.COOKIES.get('username', '')
     if 'search' in request.GET.keys() or 'q' in request.GET.keys():
         key_word = request.GET.get('q')
@@ -171,7 +171,6 @@ def build_lib(request):
 @login_required
 def upmachine(request):
     username = request.COOKIES.get('username', '')
-    #login_serach(request, username)
     (project_id, all_proj_info, sample_info, select_proj, all_attachment) = view_todo(request, table='upmachine')
 
     return render(request, os.path.join(CODE_ROOT, 'lims_app/templates', 'upmachine.html'),
@@ -192,43 +191,144 @@ def downmachine(request):
                      'select_proj': select_proj, 'project_id': project_id, 'all_attachment': all_attachment})
 
 
-@login_required
 def save_sample_info(request):
     table_name = request.GET.get('table')
     sample_id = request.POST['name']
     value = request.POST['value']
-    if table_name == 'receive_sample':
-        SampleInfoDetail.objects.filter(id=sample_id.replace('sample_id_', '')).update(sendsample_comment=value)
+    if table_name == 'send_sample':
+        SendSample.objects.filter(id=sample_id.replace('sample_id_', '')).update(comment=value)
     elif table_name == 'quality_check':
-        SampleInfoDetail.objects.filter(id=sample_id.replace('sample_id_', '')).update(qualitycheck_comment=value)
+        QualityCheck.objects.filter(id=sample_id.replace('sample_id_', '')).update(comment=value)
     elif table_name == 'build_lib':
-        SampleInfoDetail.objects.filter(id=sample_id.replace('sample_id_', '')).update(lib_comment=value)
+        BuildLib.objects.filter(id=sample_id.replace('sample_id_', '')).update(comment=value)
     elif table_name == 'upmachine':
-        SampleInfoDetail.objects.filter(id=sample_id.replace('sample_id_', '')).update(upmachine_comment=value)
+        UpMachine.objects.filter(id=sample_id.replace('sample_id_', '')).update(comment=value)
     elif table_name == 'downmachine':
-        SampleInfoDetail.objects.filter(id=sample_id.replace('sample_id_', '')).update(downmachine_comment=value)
+        DownMachine.objects.filter(id=sample_id.replace('sample_id_', '')).update(comment=value)
 
     return JsonResponse({'msg': 'ok'})
 
 
-@login_required
 def down_sample_info(request):
-    transform_title = {'quality_check': u'质检', 'receive_sample': u'送样', 'build_lib': u'建库',
+    title = {'quality_check': u'质检', 'send_sample': u'送样', 'build_lib': u'建库',
                         'upmachine': u'上机', 'downmachine': u'下机'}
 
     table_name = request.GET.get('table', '')
     project_number = request.GET.get('project_number', '')
     if table_name and project_number:
-        query_sets = SampleInfoDetail.objects.filter(project_number=project_number)
-        column_names = select_colums_dict[table_name]
-        return excel.make_response_from_query_sets(    #require excel module
-            query_sets,
-            column_names,
-            'xls',
-            file_name=project_number + transform_title[table_name] + u'结果'
-        )
+        if table_name == 'send_sample':
+            return excel.make_response_from_a_table(SendSample, 'xls', file_name=project_number + title[table_name] + u'结果')
+        elif table_name == 'quality_check':
+            return excel.make_response_from_a_table(QualityCheck, 'xls', file_name=project_number + title[table_name] + u'结果')
+        elif table_name == 'build_lib':
+            return excel.make_response_from_a_table(BuildLib, 'xls', file_name=project_number + title[table_name] + u'结果')
+        elif table_name == 'upmachine':
+            return excel.make_response_from_a_table(UpMachine, 'xls', file_name=project_number + title[table_name] + u'结果')
+        elif table_name == 'downmachine':
+            return excel.make_response_from_a_table(DownMachine, 'xls', file_name=project_number + title[table_name] + u'结果')
     else:
         JsonResponse({'msg': 'error'})
+
+
+@login_required
+def upload_sample_info(request):
+    title = {'quality_check': u'质检', 'send_sample': u'送样', 'build_lib': u'建库',
+             'upmachine': u'上机', 'downmachine': u'下机'}
+
+    username = request.COOKIES.get('username', '')
+    table_name = request.GET.get('table', '')
+    project_id = request.GET.get('project_id', '')
+    if table_name and project_id:
+        return render(request,  os.path.join(CODE_ROOT, 'lims_app/templates', 'upload.html'),
+                        {'username': username, 'table': table_name, 'table_name': title[table_name], 'project_id': project_id})
+
+
+def save_sample_row(request):
+    request
+    return JsonResponse({'msg': 'ok'})
+
+
+def save_sample_table(request):
+    json_data = request.POST['sample_table_data']
+    json_data = json.loads(json_data)
+    project_id = request.POST['project_id']
+    action = request.POST['action']
+    table_name = request.POST['table']
+
+    project_number = SampleProjectMaster.objects.get(id=project_id).project_number
+
+    if table_name == 'send_sample':
+        if action == 'update':
+            SendSample.objects.filter(project_id=project_id).delete()
+        for row_dict in json_data:
+            p = SendSample(project_number=project_number,
+                           project_id=project_id,
+                           sample_name=row_dict['sample_name'],
+                           species=row_dict['species'],
+                           express_number=row_dict['express_number'],
+                           product_num=row_dict['product_num'],
+                           time=row_dict['time'],
+                           comment=row_dict['comment'])
+            p.save()
+    elif table_name == 'quality_check':
+        if action == 'update':
+            QualityCheck.objects.filter(project_id=project_id).delete()
+        for row_dict in json_data:
+            p = QualityCheck(project_id=project_id,
+                             project_number=project_number,
+                             sample_name=row_dict['sample_name'],
+                             sample_id=row_dict['sample_id'],
+                             concentration=row_dict['concentration'],
+                             volume=row_dict['volume'],
+                             rin=row_dict['rin'],
+                             results=row_dict['results'],
+                             time=row_dict['time'],
+                             comment=row_dict['comment'])
+            p.save()
+    elif table_name == 'build_lib':
+        if action == 'update':
+            BuildLib.objects.filter(project_id=project_id).delete()
+        for row_dict in json_data:
+            p = BuildLib(project_id=project_id,
+                         project_number=project_number,
+                         sample_name=row_dict['sample_name'],
+                         sample_id=row_dict['sample_id'],
+                         lib_id=row_dict['lib_id'],
+                         time=row_dict['time'],
+                         comment=row_dict['comment'])
+            p.save()
+    elif table_name == 'upmachine':
+        if action == 'update':
+            UpMachine.objects.filter(project_id=project_id).delete()
+        for row_dict in json_data:
+            p = UpMachine(project_id=project_id,
+                          project_number=project_number,
+                          sample_name=row_dict['sample_name'],
+                          sample_id=row_dict['sample_id'],
+                          upmachinetype=row_dict['upmachinetype'],
+                          mode=row_dict['mode'],
+                          data_count=row_dict['data_count'],
+                          time=row_dict['time'],
+                          comment=row_dict['comment']
+                          )
+            p.save()
+    elif table_name == 'downmachine':
+        if action == 'update':
+            DownMachine.objects.filter(project_id=project_id).delete()
+        for row_dict in json_data:
+            p = DownMachine(project_id=project_id,
+                            project_number=project_number,
+                            sample_name=row_dict['sample_name'],
+                            sample_id=row_dict['sample_id'],
+                            q20=row_dict['q20'],
+                            q30=row_dict['q30'],
+                            data_count=row_dict['data_count'],
+                            time=row_dict['time'],
+                            comment=row_dict['comment'])
+            p.save()
+    else:
+        return JsonResponse({'msg': '操作失败!'})
+    return JsonResponse({'msg': u'操作成功!'})
 
 
 def save_table_data(request):
@@ -246,7 +346,7 @@ def save_table_data(request):
                                         sendsample_time=row_dict['修改日期'],
                                         sendsample_comment=row_dict['样品备注'])
 
-    return redirect('/lims_app/receive_sample/?project_id=%s' % project_id)
+    return redirect('/lims_app/send_sample/?project_id=%s' % project_id)
 
 
 def upload_attachment(request):
