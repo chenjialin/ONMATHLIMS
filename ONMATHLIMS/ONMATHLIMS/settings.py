fe8d92fc4196145266a8c3a28bb4c87e8ebc5f4e
chencheng <291552579@qq.com>
2017-08-07 10:07:12 +0800

    get db configure from environ

--- ONMATHLIMS/ONMATHLIMS/settings.py
+++ ONMATHLIMS/ONMATHLIMS/settings.py
@@ -84,9 +84,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'SEQ_SA_INFO',                      # Or path to database file if using sqlite3.
-        'USER': 'root',
-        'PASSWORD': '050400',
-        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'USER': os.environ.get('ONMATHLIMS_DB_USER'),
+        'PASSWORD': os.environ.get('ONMATHLIMS_DB_PASSWORD'),
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306',                      # Set to empty string for default.
     }
 }
