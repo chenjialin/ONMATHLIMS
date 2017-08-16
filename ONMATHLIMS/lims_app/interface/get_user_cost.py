bfa1cb735fd5580e048c67b026c48cb7958c6ad5
chencheng <291552579@qq.com>
2017-08-16 15:05:05 +0800

    add download function

--- ONMATHLIMS/lims_app/interface/get_user_cost.py
+++ ONMATHLIMS/lims_app/interface/get_user_cost.py
@@ -1,6 +1,11 @@
 # coding: utf8
+import os
+import sys
+import datetime
 from math import ceil
+from openpyxl import Workbook
 from django.db import connection
+
 
 cmd_dict = {'billing_info': '''select bi.* ,sm.cust_user from billing_info bi
                                inner join sample_project_master sm on bi.project_id=sm.id
@@ -11,8 +16,32 @@ cmd_dict = {'billing_info': '''select bi.* ,sm.cust_user from billing_info bi
            'cost_info': '''select ct.* ,sm.cust_user from cost_info ct
                            inner join sample_project_master sm on ct.project_id=sm.id
                            where sm.cust_user="%s"'''}
+
+billing_map = {u'项目编号': 'project_number',
+               u'金额': 'expense',
+               u'开票': 'billing_number',
+               u'时间': 'time',
+               u'备注': 'comment'}
+receipt_map = {u'项目编号': 'project_number',
+               u'金额': 'expense',
+               u'时间': 'time',
+               u'备注': 'comment'}
+cost_map = {u'项目编号': 'project_number',
+            u'金额': 'expense',
+            u'样品数量': 'sample_number',
+            u'单价': 'unit_cost',
+            u'时间': 'time',
+            u'备注': 'comment'}
+title_map = {'billing_info': billing_map,
+             'receipt_info': receipt_map,
+             'cost_info': cost_map}
+title_list = {'billing_info': [u'项目编号', u'金额', u'开票', u'时间', u'备注'],
+              'receipt_info': [u'项目编号', u'金额', u'时间', u'备注'],
+              'cost_info': [u'项目编号', u'金额', u'样品数量', u'单价', u'时间', u'备注']}
+
+
 def get_expense_info(user, limit, sidx, sord, page, table):
-    cmd1 =  cmd_dict[table] % (user)
+    cmd1 = cmd_dict[table] % (user)
     cursor = connection.cursor()
     cursor.execute(cmd1)
     results = cursor.fetchall()
@@ -29,9 +58,9 @@ def get_expense_info(user, limit, sidx, sord, page, table):
     else:
         start = int(limit) * page - int(limit)
     cmd2 = cmd1 + " order by {sort_index} {sort_direct} limit {start}, {limit}".format(sort_index=sidx,
-                                                                                      sort_direct=sord,
-                                                                                      start=int(start),
-                                                                                      limit=limit)
+                                                                                       sort_direct=sord,
+                                                                                       start=int(start),
+                                                                                       limit=limit)
     cursor = connection.cursor()
     cursor.execute(cmd2)
     results = cursor.fetchall()
@@ -50,3 +79,31 @@ def get_expense_total(user, table):
         total_expense += result[3]
 
     return total_expense
+
+
+def down_expense_table(user, table):
+    file_title = {'billing_info': u'开票信息表', 'receipt_info': u'收款信息表', 'cost_info': u'成本信息表'}
+    book = Workbook()
+    sheet1 = book.worksheets[0]
+    cmd = cmd_dict[table] % (user)
+    cursor = connection.cursor()
+    cursor.execute(cmd)
+
+    results = cursor.fetchall()
+    results = [result[2:len(result)-1] for result in results]
+
+    for i, title in enumerate(title_list[table]):
+        sheet1.cell(row=1, column=i+1).value = title
+    for i, result in enumerate(results):
+        for j, data in enumerate(result):
+            sheet1.cell(row=i+2, column=j+1).value = data
+
+    export_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static', 'export')
+
+    if not os.path.exists(export_path):
+        os.mkdir(export_path)
+
+    file_name = '_'.join([user, file_title[table]]) + datetime.datetime.now().strftime("%Y%m%d%H%M%S") + '.xls'
+    full_file_name = os.path.join(export_path, file_name)
+    book.save(filename=full_file_name)
+    return full_file_name
