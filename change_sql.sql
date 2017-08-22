618bd5d587e896601e628fc9a49f4a7a1ae453e1
chencheng <291552579@qq.com>
2017-08-22 10:59:29 +0800

    update mysql change

--- change_sql.sql
+++ change_sql.sql
@@ -41,30 +41,40 @@ add upload_time save and om_id three fields
 */
 ALTER TABLE `SEQ_SA_INFO`.`send_sample`
 CHANGE COLUMN `time` `create_time` DATETIME NULL DEFAULT NULL ,
-ADD COLUMN `upload_time` DATETIME NULL AFTER `comment`,
-ADD COLUMN `save` VARCHAR(1) NULL AFTER `upload_time`,
+ADD COLUMN `upload_time` VARCHAR(45) NULL AFTER `comment`,
+ADD COLUMN `status` VARCHAR(1) NULL AFTER `upload_time`,
 ADD COLUMN `om_id` VARCHAR(100) NULL AFTER `species`;
 
 ALTER TABLE `SEQ_SA_INFO`.`quality_check`
 CHANGE COLUMN `time` `create_time` DATETIME NULL DEFAULT NULL ,
-ADD COLUMN `upload_time` DATETIME NULL AFTER `comment`,
-ADD COLUMN `save` VARCHAR(1) NULL AFTER `upload_time`,
+ADD COLUMN `upload_time`  VARCHAR(45)  NULL AFTER `comment`,
+ADD COLUMN `status` VARCHAR(1) NULL AFTER `upload_time`,
 ADD COLUMN `om_id` VARCHAR(100) NULL AFTER `sample_name`;
 
 ALTER TABLE `SEQ_SA_INFO`.`build_lib`
 CHANGE COLUMN `time` `create_time` DATETIME NULL DEFAULT NULL ,
-ADD COLUMN `upload_time` DATETIME NULL AFTER `comment`,
-ADD COLUMN `save` VARCHAR(1) NULL AFTER `upload_time`,
+ADD COLUMN `upload_time`  VARCHAR(45)  NULL AFTER `comment`,
+ADD COLUMN `status` VARCHAR(1) NULL AFTER `upload_time`,
 ADD COLUMN `om_id` VARCHAR(100) NULL AFTER `sample_name`;
 
 ALTER TABLE `SEQ_SA_INFO`.`upmachine`
 CHANGE COLUMN `time` `create_time` DATETIME NULL DEFAULT NULL ,
-ADD COLUMN `upload_time` DATETIME NULL AFTER `comment`,
-ADD COLUMN `save` VARCHAR(1) NULL AFTER `upload_time`,
+ADD COLUMN `upload_time`  VARCHAR(45)  NULL AFTER `comment`,
+ADD COLUMN `status` VARCHAR(1) NULL AFTER `upload_time`,
 ADD COLUMN `om_id` VARCHAR(100) NULL AFTER `sample_name`;
 
 ALTER TABLE `SEQ_SA_INFO`.`downmachine`
 CHANGE COLUMN `time` `create_time` DATETIME NULL DEFAULT NULL ,
-ADD COLUMN `upload_time` DATETIME NULL AFTER `comment`,
-ADD COLUMN `save` VARCHAR(1) NULL AFTER `upload_time`,
+ADD COLUMN `upload_time`  VARCHAR(45)  NULL AFTER `comment`,
+ADD COLUMN `status` VARCHAR(1) NULL AFTER `upload_time`,
 ADD COLUMN `om_id` VARCHAR(100) NULL AFTER `sample_name`;
+/* 2017-08-21
+*/
+ALTER TABLE `SEQ_SA_INFO`.`quality_check`
+DROP INDEX `sample_id_UNIQUE` ;
+ALTER TABLE `SEQ_SA_INFO`.`build_lib`
+DROP INDEX `sample_id_UNIQUE` ;
+ALTER TABLE `SEQ_SA_INFO`.`upmachine`
+DROP INDEX `sample_id_UNIQUE` ;
+ALTER TABLE `SEQ_SA_INFO`.`downmachine`
+DROP INDEX `sample_id_UNIQUE` ;
