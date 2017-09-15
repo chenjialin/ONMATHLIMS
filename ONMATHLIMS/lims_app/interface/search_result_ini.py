647ac65b3b22dbc227b5c35a4e9ae0c62c3abd25
chencheng <291552579@qq.com>
2017-09-15 11:10:32 +0800

    fix search sql cmd bug

--- ONMATHLIMS/lims_app/interface/search_result_ini.py
+++ ONMATHLIMS/lims_app/interface/search_result_ini.py
@@ -4,6 +4,7 @@ from django.db import connection
 
 def get_search_result(search_key):
     cursor = connection.cursor()
+
     sql = """SELECT
             m.project_name as "项目名称",
             m.project_number as "项目编码",
@@ -23,28 +24,28 @@ def get_search_result(search_key):
     sql2 = """select
               ss.project_number,
               ss.sample_name,
+              ss.sample_id,
               ss.om_id,
               ss.species,
-              ss.status as ss_st,
-              qc.sample_id,
               qc.results,
-              qc.status as qc_st,
               um.data_count,
-              um.status as um_st
+              dm.data_count
               from send_sample ss
-              left join quality_check qc on ss.om_id=qc.om_id
-              left join upmachine um on um.om_id=ss.om_id
+              left join quality_check qc on ss.sample_id=qc.sample_id
+              left join upmachine um on qc.sample_id=um.sample_id
+              left join downmachine dm on um.sample_id=dm.sample_id
            """
-    condition = " where ss.project_number='{key}' or ss.sample_name='{key}' or ss.species='{key}' or qc.sample_id='{key}'"
+    condition = """
+                where (ss.project_number='{key}' or ss.sample_name='{key}' or ss.species='{key}' or qc.sample_id='{key}')
+                and ss.status='Y' and (qc.status='Y' or qc.status is NULL)
+                and (um.status='Y' or um.status is NULL)
+                and (dm.status='Y' or dm.status is NULL)
+                """
     sql2 += condition.format(key=search_key)
     cursor.execute(sql2)
     results = cursor.fetchall()
-    out_results = []
-    for result in results:
-        if result[2] and result[4] != 'N' and result[7] != 'N' and result[9] != 'N':
-            out_results.append(result)
 
-    table_header = ['项目编号', '样品名称', 'OMID', '物种', '样品编号', '判定结果', '上机数据量']
-    table_values = out_results
+    table_header = ['项目编号', '样品名称', '样品编号', 'OMID', '物种', '判定结果', '上机数据量', '下机数据量']
+    table_values = results
 
     return table_header, table_values
