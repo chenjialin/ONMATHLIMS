e126b63960d36578c0910dac3309af875eb5c6a2
陈佳林 <chenjialin@9ffenqigo.com>
2017-09-27 20:07:45 +0800

    version 1.2.0

--- ONMATHLIMS/ONMATHLIMS/settings.py
+++ ONMATHLIMS/ONMATHLIMS/settings.py
@@ -84,8 +84,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'SEQ_SA_INFO',                      # Or path to database file if using sqlite3.
-        'USER': os.environ.get('ONMATHLIMS_DB_USER', 'root'),
-        'PASSWORD': os.environ.get('ONMATHLIMS_DB_PASSWORD', '050400'),
+        'USER': os.environ.get('ONMATHLIMS_DB_USER', 'onmaisiadmin'),
+        'PASSWORD': os.environ.get('ONMATHLIMS_DB_PASSWORD', 'onmaisiadmin'),
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306',                      # Set to empty string for default.
     }
